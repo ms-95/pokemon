640ddfae85f4e199ab9ae6ea58eb0d201856c0dc
Pang Siu Fung <mesongpang@gmail.com>
2021-02-15 17:55:33 +0800

    style: correct spinner background for safari

--- src/shared/components/spinner.component.tsx
+++ src/shared/components/spinner.component.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import SpinnerGif from '../../assets/images/spinner.gif';
 export default function Spinner() {
     return <React.Fragment>
-        <div className="position-fixed" style={{ top: 0, bottom: 0, left: 0, right: 0,  zIndex: 8000, backdropFilter: 'blur(10px)' }}>
+        <div className="position-fixed" style={{ top: 0, bottom: 0, left: 0, right: 0,  zIndex: 8000, backdropFilter: 'blur(10px)', WebkitBackdropFilter: 'blur(10px)' }}>
         </div>
 
         <div className="d-flex align-items-center justify-content-center position-fixed" style={{ top: 0, bottom: 0, left: 0, right: 0, zIndex: 9999 }}>
